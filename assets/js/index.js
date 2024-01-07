cc0a371e17f231f2560d465219c69f116bdb2b45
JoshuaT <jttsat170@gmail.com>
2024-01-07 15:42:03 -0500

    adding more answer options and updating id

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,8 +6,8 @@ var button1;
 var button2;
 var button3;
 var button4;
-var questionArray = ["What is a variable"];
-var answerArray = ["Words"];
+var questionArray = ["What is a variable", "What is an array", "What is a modulus", "What is an object"];
+var answerArray = ["A name that holds a value", "It is an expression", "A theory", "A function"];
 
 startButton.addEventListener("click", startQuiz);
 
@@ -22,17 +22,17 @@ function startQuiz(){
     button4 = document.createElement("button");
     newDiv.setAttribute('class', 'container');
     button1.setAttribute('class', 'choice')
-    button1.setAttribute('id', 'choice');
+    button1.setAttribute('id', 'button1');
     button2.setAttribute('class', 'choice');
-    button2.setAttribute('id', 'back');
+    button2.setAttribute('id', 'button2');
     button3.setAttribute('class', 'choice');
-    button3.setAttribute('id', 'me');
+    button3.setAttribute('id', 'button3');
     button4.setAttribute('class', 'choice');
-    button4.setAttribute('id', 'joy');
+    button4.setAttribute('id', 'button4');
     var textNode = document.createTextNode(answerArray[0]);
-    var textNode2 = document.createTextNode(answerArray[0]);
-    var textNode3 = document.createTextNode(answerArray[0]);
-    var textNode4 = document.createTextNode(answerArray[0]);
+    var textNode2 = document.createTextNode(answerArray[1]);
+    var textNode3 = document.createTextNode(answerArray[2]);
+    var textNode4 = document.createTextNode(answerArray[3]);
     var questionText = document.createTextNode(questionArray[0]);
     button1.appendChild(textNode);
     button2.appendChild(textNode2);
@@ -45,4 +45,16 @@ function startQuiz(){
     newDiv.appendChild(button2);
     newDiv.appendChild(button3);
     newDiv.appendChild(button4);
+}
+
+function generateQuestion(){
+
+}
+
+function startTimer(counter){
+
+}
+
+function getHighScore(){
+
 }
